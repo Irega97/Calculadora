be91d389ccead13166c7036bfec270b3b0562cf7
irega97 <ivanreq97@gmail.com>
2020-03-26 23:58:51 +0100

    Declaración operaciones

--- app/src/main/java/edu/upc/dsa/calculadora/MainActivity.java
+++ app/src/main/java/edu/upc/dsa/calculadora/MainActivity.java
@@ -12,55 +12,182 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    double operacion1;
-    double operacion2;
-    double result;
-    int igual=0;
-    int suma=0;
-    int resta=0;
-    int multiplicacion=0;
-    int division=0;
-    boolean inicio;
-    String operacion="";
+    public TextView pantalla;
+    public double op1, op2, res;
+    int ope;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView pantalla = findViewById((R.id.pantalla));
-        final Button btn1 = findViewById(R.id.button1);
-        final Button btn2 = findViewById(R.id.button2);
-        final Button btn3 = findViewById(R.id.button3);
-        final Button btn4 = findViewById(R.id.button4);
-        final Button btn5 = findViewById(R.id.button5);
-        final Button btn6 = findViewById(R.id.button6);
-        final Button btn7 = findViewById(R.id.button7);
-        final Button btn8 = findViewById(R.id.button8);
-        final Button btn9 = findViewById(R.id.button9);
-        final Button btn0 = findViewById(R.id.button0);
-        final Button btn_igual = findViewById(R.id.button_igual);
-        final Button btn_suma = findViewById(R.id.button_suma);
-        final Button btn_resta = findViewById(R.id.button_resta);
-        final Button btn_prod = findViewById(R.id.button_prod);
-        final Button btn_delete = findViewById(R.id.button_clear);
-        final Button btn_division = findViewById(R.id.button_division);
-        final Button btn_punto = findViewById(R.id.button_punto);
-        final Button btn_sen = findViewById(R.id.button_sen);
-        final Button btn_cos = findViewById(R.id.button_cos);
-        final Button btn_tan = findViewById(R.id.button_tan);
-        final Button btn_grados= findViewById(R.id.button_grados);
+        pantalla=(TextView)findViewById(R.id.pantalla);
+    }
 
-        btn0.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (inicio) {
-                    pantalla.setText(pantalla.getText().toString() + btn0.getText().toString());
-                } else {
-                    pantalla.setText(btn0.getText().toString());
-                    inicio = true;
-                }
-            }
-        });
+    public void bt1 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"1";
+        pantalla.setText(cap);
+    }
+
+    public void bt2 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"2";
+        pantalla.setText(cap);
+    }
+
+    public void bt3 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"3";
+        pantalla.setText(cap);
+    }
+
+    public void bt4 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"4";
+        pantalla.setText(cap);
+    }
+
+    public void bt5 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"5";
+        pantalla.setText(cap);
+    }
+
+    public void bt6 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"6";
+        pantalla.setText(cap);
+    }
+
+    public void bt7 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"7";
+        pantalla.setText(cap);
+    }
+
+    public void bt8 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"8";
+        pantalla.setText(cap);
+    }
+
+    public void bt9 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"9";
+        pantalla.setText(cap);
+    }
+
+    public void bt0 (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+"0";
+        pantalla.setText(cap);
+    }
+
+    public void bt_punto (View v){
+        String cap=pantalla.getText().toString();
+        cap=cap+".";
+        pantalla.setText(cap);
+    }
+
+    public void suma (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+            pantalla.setText(""); //Limpiamos pantalla
+            ope = 1; //Identificador operacion
+    }
+
+    public void resta (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText(""); //Limpiamos pantalla
+        ope = 2; //Identificador operacion
+    }
+
+    public void multiplicacion (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText(""); //Limpiamos pantalla
+        ope = 3; //Identificador operacion
+    }
+
+    public void division (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText(""); //Limpiamos pantalla
+        ope = 4; //Identificador operacion
+    }
+
+    public void sen (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText("sen("+op1+")"); //Actualizamos pantalla
+        ope = 5; //Identificador operacion
+    }
+
+    public void cos (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText("cos("+op1+")"); //Actualizamos pantalla
+        ope = 6; //Identificador operacion
+    }
+
+    public void tan (View v){
+        //Añadimos try/catch para que no haya error si pulsamos operacion sin introducir numeros
+        try { //Obtenemos primer valor
+            String aux1 = pantalla.getText().toString();
+            op1 = Double.parseDouble(aux1);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText("tan("+op1+")"); //Actualizamos pantalla
+        ope = 7; //Identificador operacion
+    }
+
+    public void igual (View v){
+        //Añadimos try/catch para op2 para que no haya error en las operaciones trigonometricas
+        try { //Obtenemos primer valor
+            String aux2 = pantalla.getText().toString();
+            op2 = Double.parseDouble(aux2);
+        }catch (NumberFormatException nfe){}
+        pantalla.setText(""); //Limpiamos pantalla
+
+        if(ope==1)
+            res=op1+op2;
+        else if(ope==2)
+            res=op1-op2;
+        else if(ope==3)
+            res=op1*op2;
+        else if(ope==4){
+            if(op2==0)
+                pantalla.setText("Math error");
+            else
+                res=op1/op2;
+        }else if(ope==5){
+            double rad=Math.toRadians(op1);
+            res=Math.sin(rad);
+        }else if(ope==6){
+            double rad=Math.toRadians(op1);
+            res=Math.cos(rad);
+        }else if(ope==7){
+            double rad=Math.toRadians(op1);
+            res=Math.tan(rad);
+        }
     }
 }
